6e16a6a2548f62cb1cad0701153fc099875e84ca
Denis Molony <dmolony@users.noreply.github.com>
2024-05-02 15:35:31 +1000

    match data fork

--- src/com/bytezone/appleformat/file/ResourceFile.java
+++ src/com/bytezone/appleformat/file/ResourceFile.java
@@ -2,12 +2,13 @@ package com.bytezone.appleformat.file;
 
 import com.bytezone.appleformat.Utility;
 import com.bytezone.filesystem.AppleFile;
+import com.bytezone.filesystem.FileProdos;
+import com.bytezone.filesystem.ForkProdos;
 
 // -----------------------------------------------------------------------------------//
 public class ResourceFile extends AbstractFormattedAppleFile
 // -----------------------------------------------------------------------------------//
 {
-
   // ---------------------------------------------------------------------------------//
   public ResourceFile (AppleFile appleFile, byte[] buffer)
   // ---------------------------------------------------------------------------------//
@@ -31,8 +32,21 @@ public class ResourceFile extends AbstractFormattedAppleFile
       return "This file has no data\n\n" + appleFile.getErrorMessage ();
 
     StringBuilder text = new StringBuilder ();
+    FileProdos file;
+    if (appleFile instanceof ForkProdos forkProdos)
+      file = forkProdos.getParentFile ();
+    else
+      file = (FileProdos) appleFile;
 
-    text.append (String.format ("Name .............. %-15s%n", appleFile.getFileName ()));
+    text.append (
+        String.format ("File name .............. %-15s%n", appleFile.getFileName ()));
+    text.append (String.format ("File type .............. %02X  %s%n",
+        appleFile.getFileType (), appleFile.getFileTypeText ()));
+    text.append (String.format ("Aux .................... %04X%n", file.getAuxType ()));
+    text.append (
+        String.format ("Blocks ................. %,9d%n", appleFile.getTotalBlocks ()));
+    text.append (
+        String.format ("EOF .................... %,9d%n", appleFile.getFileLength ()));
 
     return Utility.rtrim (text);
   }
