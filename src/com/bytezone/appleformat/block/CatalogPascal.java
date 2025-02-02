c7c144a0df0983eb02eff938bbde198676a53941
Denis Molony <dmolony@users.noreply.github.com>
2025-02-02 13:54:40 +1000

    FileSystem changes

--- src/com/bytezone/appleformat/block/CatalogPascal.java
+++ src/com/bytezone/appleformat/block/CatalogPascal.java
@@ -3,7 +3,6 @@ package com.bytezone.appleformat.block;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.List;
 
 import com.bytezone.appleformat.HexFormatter;
 import com.bytezone.appleformat.Utility;
@@ -33,8 +32,8 @@ public class CatalogPascal extends AbstractFormattedAppleBlock
   // ---------------------------------------------------------------------------------//
   {
     FsPascal fsPascal = (FsPascal) appleBlock.getFileSystem ();
-    List<AppleBlock> blocks = fsPascal.getCatalogBlocks ();
-    byte[] buffer = fsPascal.readBlocks (blocks);
+    //    List<AppleBlock> blocks = fsPascal.getCatalogBlocks ();
+    byte[] buffer = fsPascal.getCatalogBuffer ();
 
     StringBuilder text = getHeader ("Pascal Catalog Sector");
 
@@ -56,34 +55,43 @@ public class CatalogPascal extends AbstractFormattedAppleBlock
     addText (text, buffer, 20, 2, "Most recent date setting : " + date);
     addTextAndDecimal (text, buffer, 22, 4, "Reserved");
 
-    int ptr = CATALOG_ENTRY_SIZE;
+    int ptr = CATALOG_ENTRY_SIZE;                     // skip the volume entry
     int totalFiles = Utility.getShort (buffer, 16);
 
     while (ptr < buffer.length && totalFiles > 0)
     {
-      if (buffer[ptr + 6] == 0)
+      if (buffer[ptr + 6] == 0)                           // no name
         break;
+
       text.append ("\n");
+
       addTextAndDecimal (text, buffer, ptr + 0, 2, "File's first block");
       addTextAndDecimal (text, buffer, ptr + 2, 2, "File's last block");
+
       int type = buffer[ptr + 4] & 0x0F;
       if (type < fileTypes.length)
         addText (text, buffer, ptr + 4, 1, "File type : " + fileTypes[type]);
+
       int wildcard = buffer[ptr + 4] & 0xC0;
       addText (text, buffer, ptr + 5, 1, "Wildcard : " + wildcard);
+
       name = HexFormatter.getPascalString (buffer, ptr + 6);
       addText (text, buffer, ptr + 6, 4, "");
       addText (text, buffer, ptr + 10, 4, "");
       addText (text, buffer, ptr + 14, 4, "");
       addText (text, buffer, ptr + 18, 4, "File name : " + name);
+
       addTextAndDecimal (text, buffer, ptr + 22, 2, "Bytes in file's last block");
 
       localDate = Utility.getPascalLocalDate (buffer, ptr + 24);
       date = localDate == null ? "--" : localDate.format (dtf);
       addText (text, buffer, ptr + 24, 2, "Date : " + date);
 
+      int firstBlock = Utility.getShort (buffer, ptr);
+      if (firstBlock > 0)                                 // deleted files don't count
+        --totalFiles;
+
       ptr += CATALOG_ENTRY_SIZE;
-      --totalFiles;                           // what if there are deleted files?
     }
 
     return Utility.rtrim (text);
